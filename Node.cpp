ae69e926be240ab74bb7e030609613f27af5341d
Lennard Kittner <lennardkittner@icloud.com>
2022-10-27 16:54:34 +0200

    Moved implementation from `Node.h` to `Node.cpp`

--- Node.cpp
+++ Node.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "Node.h"
+
+Node::Node(int value0){
+    this->value0 = value0;
+    this->value1 = 0;
+    this->singleNode = true;
+    value = value0;
+}
+Node::Node(Node* node0, Node* node1){
+    this->node0 = node0;
+    this->node1 = node1;
+    value = node0->getValue()+node1->getValue();
+}
+
+void Node::setValue0(int value0){
+    this->value0 = value0;
+}
+void Node::setValue1(int value1){
+    this->value1 = value1;
+}
+void Node::setNode0(Node node0){
+    this->node0 = &node0;
+}
+void Node::setNode1(Node node1){
+    this->node1 = &node1;
+}
+int Node::getValue0(){
+    return value0;
+}
+int Node::getValue1(){
+    return value1;
+}
+Node* Node::getNode0(){
+    return node0;
+}
+Node* Node::getNode1(){
+    return node1;
+}
+void Node::setValue(int value){
+    this->value = value;
+}
+int Node::getValue(){
+    return value;
+}
+int Node::compareTo(Node n){
+    return getValue()-n.getValue();
+}
+bool Node::isSingleNode(){
+    return singleNode;
+}
--- Node.h
+++ Node.h
@@ -10,53 +10,20 @@
 
 class Node {
 public:
-    void setValue0(int value0){
-        this->value0 = value0;
-    }
-    void setValue1(int value1){
-        this->value1 = value1;
-    }
-    void setNode0(Node node0){
-        this->node0 = &node0;
-    }
-    void setNode1(Node node1){
-        this->node1 = &node1;
-    }
-    int getValue0(){
-        return value0;
-    }
-    int getValue1(){
-        return value1;
-    }
-    Node* getNode0(){
-        return node0;
-    }
-    Node* getNode1(){
-        return node1;
-    }
-    void setValue(int value){
-        this->value = value;
-    }
-    int getValue(){
-        return value;
-    }
-    int compareTo(Node n){
-        return getValue()-n.getValue();
-    }
-    bool isSingleNode(){
-        return singleNode;
-    }
-    Node(int value0){
-        this->value0 = value0;
-        this->value1 = 0;
-        this->singleNode = true;
-        value = value0;
-    }
-    Node(Node* node0, Node* node1){
-        this->node0 = node0;
-        this->node1 = node1;
-        value = node0->getValue()+node1->getValue();
-    }
+    void setValue0(int);
+    void setValue1(int);
+    void setNode0(Node);
+    void setNode1(Node);
+    int getValue0();
+    int getValue1();
+    Node* getNode0();
+    Node* getNode1();
+    void setValue(int);
+    int getValue();
+    int compareTo(Node);
+    bool isSingleNode();
+    Node(int);
+    Node(Node*, Node*);
 
 private:
     int value0;
